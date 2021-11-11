0d245de199b5a6919a43d25e769c5568c1a0897f
Nathan Brouwer <brouwern@gmail.com>
2021-11-11 12:15:54 -0500

    new ch

---
+++ _TODO_TODO_TODO.R
@@ -0,0 +1,48 @@
+Talking glossary
+Candidate gene
+Knockout
+Haplotype
+Genetic marker
+Locus
+
+Wikipedia:
+GWAS
+Manhattan plot
+Microarray
+
+
+Medline:
+SNPs
+GWAS
+
+
+
+https://medlineplus.gov/genetics/understanding/genomicresearch/encode/
+
+https://www.genome.gov/genetics-glossary/BRCA1-BRCA2
+https://www.genome.gov/genetics-glossary/Exon
+https://www.genome.gov/genetics-glossary/Centimorgan
+https://www.genome.gov/genetics-glossary/Copy-Number-Variation
+https://www.genome.gov/genetics-glossary/Crossing-Over
+https://www.genome.gov/genetics-glossary/Cytogenetics
+https://www.genome.gov/genetics-glossary/Gene-Environment-Interaction
+https://www.genome.gov/genetics-glossary/Genetic-Map
+https://www.genome.gov/genetics-glossary/homologous-recombination
+https://www.genome.gov/genetics-glossary/hybridization
+
+
+https://www.genome.gov/genetics-glossary/Microsatellite
+
+https://www.genome.gov/genetics-glossary/Personalized-Medicine
+
+https://www.genome.gov/genetics-glossary/Physical-Map
+
+https://www.genome.gov/genetics-glossary/Polygenic-Trait
+
+https://www.genome.gov/genetics-glossary/Tandem-Repeat
+
+https://www.genome.gov/genetics-glossary/hapmap
+https://www.genome.gov/genetics-glossary/LOD-Score
+https://www.genome.gov/genetics-glossary/Mapping
+
+https://www.genome.gov/genetics-glossary/Linkage
